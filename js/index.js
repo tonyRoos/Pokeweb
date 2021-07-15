44b8444e07bc84a05a14592a5ae015d46b07d3f5
Marco Antônio M. Roos <marcos.roos.1958@gmail.com>
2021-07-15 11:28:15 -0300

    Update Temporary Alert
    The alert serves to make me understand how browser language works.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 console.log("Observação : Esta versão imprime informações no console log a fim de comprovar a funcionalidade de salvar a lista de pokemons acessados via 'estou com sorte'");
 console.log(localStorage.getItem('randomId'));
+alert ("The language is: " + navigator.language);
 
 let pokeApi = new PokeApi();
 let render = new Render();
